9425039f658ff17fb860c71fa906e531c9abac02
modularjon <modularjon@gmail.com>
2016-07-12 19:05:46 -0400

    Hard code test data for createOrder
    Can now test interaction with server for 1st step in promise chain

--- assets/scripts/stripe/stripe-events.js
+++ assets/scripts/stripe/stripe-events.js
@@ -3,7 +3,23 @@
 const api = require('./stripe-api');
 const ui = require('./stripe-ui');
 
-let shoppingCartArray = [];
+let shoppingCartArray = {
+  "order": {
+    "items": [
+      {
+        "product_id": "578496f937f3cb1e4f3b59d4",
+        "price": 10,
+        "quantity": 1
+      },
+      {
+        "product_id": "5784961b37f3cb1e4f3b59d3",
+        "price": 20,
+        "quantity": 2
+      }
+    ],
+    "total": 50.00
+  }
+};
 
 const onSendCardData = () => {
 
@@ -13,15 +29,15 @@ const onPurchaseSubmit = (event) => {
   event.preventDefault();
   let data = shoppingCartArray;
 
-  api.createOrder(data)
-    .then(onSendCardData)
-    .then(api.updateOrder)
-    .then(ui.createOrderSuccess)
-    .fail(ui.createOrderFail);
+  api.createOrder(data);
+    // .then(onSendCardData)
+    // .then(api.updateOrder)
+    // .then(ui.createOrderSuccess)
+    // .fail(ui.createOrderFail);
 };
 
 const addHandlers = () => {
-  $('.purchase').on('submit', onPurchaseSubmit);
+  $('.purchase').on('click', onPurchaseSubmit);
 };
 
 module.exports = {
