d626d6d08af3b92530e593b0446b492ef04474a5
acobrelo <acobrelo@gmail.com>
2016-07-12 01:16:43 -0400

    Created folders for auth and items ABL
    Made folders and events/ui/api files for auth and items to start
    on populating the modal that will pop up for a given item when clicked.

---
+++ assets/scripts/item/item-events.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//const getFormFields = require('../../../lib/get-form-fields');
+
+const onShowItem = function (event) {
+  event.preventDefault();
+  let item = $(this).attr('id');
+  $('#' + item);
+  //from here, retrieve ID info and then
+  //funnel it into handlebars to populate the modal
+};
+
+const addItemHandlers = () => {
+  $('.img').on('click', onShowItem);
+};
+
+module.exports = {
+  addItemHandlers,
+};
