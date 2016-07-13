f9b38b50b98ffffbd79bf13f02289fcd545b64f5
modularjon <modularjon@gmail.com>
2016-07-13 15:39:41 -0400

    Add new function addStripeChargeToOrder
    Creates new promise that resolves as an ajax request to the app.host/charge
    route. The backend should use the data to add the stripe token to the previously
    created order.

--- assets/scripts/stripe/stripe-api.js
+++ assets/scripts/stripe/stripe-api.js
@@ -19,6 +19,28 @@ const createOrder = (data) => {
   });
 };
 
+const addStripeChargeToOrder = (data) => {
+  console.log(data);
+  return new Promise((resolve, reject) => {
+    return $.ajax({
+      url: app.host + '/charge',
+      method: "POST",
+      // headers: {
+      //   Authorization: 'Token token=' + app.user.token,
+      // },
+      // dataType: 'json',
+      data,
+      success: (response) => {
+        resolve(response);
+      },
+      error: (error) => {
+        reject(error);
+      },
+    });
+  });
+};
+
 module.exports = {
   createOrder,
+  addStripeChargeToOrder,
 };
