3a5dedfb0914ed4a41f92fd3131eb422189b7648
acobrelo <acobrelo@gmail.com>
2016-07-13 14:47:47 -0400

    Added logic to populate cart ABL
    Added basic logic to populate the cart with data from the
    SHOW for a single product when "add to cart" button is clicked. Next is handlebars.

--- assets/scripts/cart/cart_storage.js
+++ assets/scripts/cart/cart_storage.js
@@ -1,3 +1,15 @@
 'use strict';
 
-let cart = [];
+
+const addItems = function () {
+  let cartItem = [];
+  for (let i = 1; i < 5; i++) {
+    let field = document.getElementById("form")[i];
+    cartItem.push(field.value);
+  }
+  console.log(cartItem);
+};
+
+module.exports = {
+  addItems,
+};
