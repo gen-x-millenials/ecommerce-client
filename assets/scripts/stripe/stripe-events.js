a691d6da4e2dc89a876621b3c86fe13d156ebe75
modularjon <modularjon@gmail.com>
2016-07-13 14:38:26 -0400

    Add functions and handlers to enable custom stripe
    Add function cartTotal to send amount data to stripe
    
    Declare handler to configure stripe with api test key for our app
    
    Declare function onCheckout to start promise chain creating an order
    
    addHandlers to on document ready

--- assets/scripts/stripe/stripe-events.js
+++ assets/scripts/stripe/stripe-events.js
@@ -21,23 +21,46 @@ let shoppingCartArray = {
   }
 };
 
-const onSendCardData = () => {
-
+const cartTotal = () => {
+  let sum = 0;
+  shoppingCartArray.order.items.forEach((item) => {
+    sum += item.price * item.quantity;
+  });
+  return sum * 100;
 };
 
-const onPurchaseSubmit = (event) => {
+let handler = StripeCheckout.configure({
+    key: 'pk_test_GkNup9bDIq38PpNXXeHBDjsL',
+    image: '/img/documentation/checkout/marketplace.png',
+    locale: 'auto',
+    token: function(token) {
+      let credentials = {
+      stripeToken: token.id,
+      amount: cartTotal()
+    };
+  }
+});
+
+const onCheckout = (event) => {
   event.preventDefault();
   let data = shoppingCartArray;
-
-  api.createOrder(data);
-    // .then(onSendCardData)
+  api.createOrder(data)
+    .then(() => handler.open({
+        name: 'Gen X and the Millenials',
+        description: 'purchase',
+        amount: cartTotal()
+      }))
+    // .then(api.stripeCharge(authUi.stripeSuccess, authUi.failure, credentials));
     // .then(api.updateOrder)
     // .then(ui.createOrderSuccess)
-    // .fail(ui.createOrderFail);
+    .catch(ui.failure);
 };
 
 const addHandlers = () => {
-  $('.purchase').on('click', onPurchaseSubmit);
+  $('#checkout-button').on('click', onCheckout);
+  $(window).on('popstate', function() {
+    handler.close();
+  });
 };
 
 module.exports = {
