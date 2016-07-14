3e8717b26e1d00c10121f7ce5048d07a44620de3
modularjon <modularjon@gmail.com>
2016-07-14 11:47:49 -0400

    Add function onSaveOrder -jj
    Saves a users cart without checkout through stripe
    
    Add callback changePaidStatus for the closed option of stripe configuration
    handler

--- assets/scripts/stripe/stripe-events.js
+++ assets/scripts/stripe/stripe-events.js
@@ -18,7 +18,7 @@ let shoppingCartArray = {
         "quantity": 2
       }
     ],
-    "total": 50.00
+    "total": 50.00,
   }
 };
 
@@ -34,14 +34,30 @@ let handler = StripeCheckout.configure({
   key: 'pk_test_GkNup9bDIq38PpNXXeHBDjsL',
   image: '/img/documentation/checkout/marketplace.png',
   locale: 'auto',
+  closed: function() {
+    console.log('done!!');
+    console.log(app.order);
+    api.changePaidStatus().then(ui.changePaidStatusSuccess).catch(ui.failure);
+  },
   token: function(token) {
     let credentials = {
       stripeToken: token.id,
       amount: cartTotal()
     };
-    api.addStripeChargeToOrder(credentials);
+    api.addStripeCharge(credentials).then(ui.success).catch(ui.failure);
   }
 });
+
+const onSaveOrder = (event) => {
+  event.preventDefault();
+  if (!app.user) {
+    return;
+  }
+  let data = shoppingCartArray;
+  api.createOrder(data)
+    .then(ui.createOrderSuccess)
+    .catch(ui.failure);
+};
 
 const onCheckout = (event) => {
   event.preventDefault();
@@ -50,16 +66,24 @@ const onCheckout = (event) => {
   }
   let data = shoppingCartArray;
   api.createOrder(data)
-    .then(() => handler.open({
-        name: 'Gen X and the Millenials',
-        description: 'purchase',
-        amount: cartTotal()
-    }))
-    // .then(ui.createOrderSuccess)
+    .then(ui.createOrderSuccess)
     .catch(ui.failure);
+
+  console.log(app.order);
+  // opens stripe checkout
+  handler.open({
+    name: 'Gen X and the Millenials',
+    description: 'purchase',
+    amount: cartTotal()
+  });
+
+  // api.changePaidStatus(data)
+  //   .then(ui.changePaidStatusSuccess)
+  //   .catch(ui.failure);
 };
 
 const addHandlers = () => {
+  $('#save-order-button').on('click', onSaveOrder);
   $('#checkout-button').on('click', onCheckout);
   $(window).on('popstate', function() {
     handler.close();
