c12b1dab2b8b7e39129d76fa41b293618900f639
modularjon <modularjon@gmail.com>
2016-07-14 11:52:20 -0400

    Add createOrderSuccess, changePaidStatusSuccess
    createOrderSuccess assigns response.order to app.order
    
    Both functions console log ajax request response

--- assets/scripts/stripe/stripe-ui.js
+++ assets/scripts/stripe/stripe-ui.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const app = require('../app.js');
+
 const success = (data) => {
   if (data) {
     console.log(data);
@@ -12,7 +14,18 @@ const failure = (error) => {
   console.error(error);
 };
 
+const createOrderSuccess = (response) => {
+  app.order = response.order;
+  console.log(app.order);
+};
+
+const changePaidStatusSuccess = (response) => {
+  console.log(response);
+};
+
 module.exports = {
   success,
   failure,
+  createOrderSuccess,
+  changePaidStatusSuccess,
 };
