416ec81d619a7d6c895033f0a4a010de502095bf
mattb5 <mattbuffardi@gmail.com>
2016-07-14 10:42:34 -0400

    completely working get history function on ..
    profile modals

--- assets/scripts/order/order_ui.js
+++ assets/scripts/order/order_ui.js
@@ -17,19 +17,22 @@ const getOrderSuccess = (data) => {
   //////////////////////////////////////////////////////////
   if (app.orders.length !== 0)
   {
-    let orderHistory = "<table> <tr> <th> Delete </th> <th> Total Cost ($) </th> <th> Item </th> <th> Quantity </th> </tr>";
+    //with delete column
+    // let orderHistory = "<table> <tr> <th> Delete </th> <th> Total Cost ($) </th> <th> Item </th> <th> Quantity </th> </tr>";
+    //without delete column  -- to re add delete button, add the <tr> tag to the first order history
+    let orderHistory = "<table> <tr> <th> Total Cost ($) </th> <th> Item </th> <th> Quantity </th> </tr>";
 
       for (let i = 0; i < app.orders.length; i++)
         {
-          orderHistory = orderHistory + "<tr><td> <button class='deleteButtons' data-order-id='" + app.orders[i]._id + "'>Delete</button>" + "</td>";
-          orderHistory = orderHistory + "<td>" + app.orders[i].total + "</td>";
+          // orderHistory = orderHistory + "<td> <button class='deleteButtons' data-order-id='" + app.orders[i]._id + "'>Delete</button>" + "</td>";
+          orderHistory = orderHistory + "<tr><td>" + app.orders[i].total + "</td>";
             for(let j=0; j<app.orders[i].items.length;j++)
                                         {
                                         orderHistory = orderHistory + "<td>" + app.orders[i].items[j].name + "</td>";
                                         orderHistory = orderHistory + "<td>" + app.orders[i].items[j].quantity + "</td></tr>";
                                         if (j+1 < app.orders[i].items.length)
                                           {
-                                            orderHistory = orderHistory + "<tr><td></td><td></td>"
+                                            orderHistory = orderHistory + "<tr><td></td>"
                                           }
                                         }
         }
